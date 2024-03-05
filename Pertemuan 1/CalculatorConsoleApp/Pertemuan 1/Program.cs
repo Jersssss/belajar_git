4e74b752d66a622062f3fa4837ba0ff126b9f383
Jersssss <jericosiahay07@students.amikom.ac.id>
2024-03-05 08:04:17 +0700

    first commit~

---
+++ Pertemuan 1/CalculatorConsoleApp/Pertemuan 1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator";
+
+            Console.WriteLine("Pilih Menu Calculator); \n");
+
+            Console.WriteLine("1. Penjumlahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.WriteLine("3. Perkalian");
+            Console.WriteLine("4. Pembagian");
+
+            Console.WriteLine("\nInput Nomor menu [1 - 4] : ");
+            int menu = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nInputkan nilai a = ");
+            int a = int.Parse(Console.ReadLine()); // proses casting
+
+            Console.WriteLine("Inputkan nilai b = ");
+            int b = Convert.ToInt32(Console.ReadLine()); // proses casting
+
+            if (menu == 1)
+            {
+                Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + penambahan(a, b));
+            }
+            else if (menu == 2)
+            {
+                Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            }
+            else if (menu == 3)
+            {
+                Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
+            }
+            else if (menu == 4)
+            {
+                Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+            }
+            else
+            {
+                Console.WriteLine("\nMaaf, menu yang anda pilih tidak tersedia");
+            }
+
+            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            Console.ReadKey();
+        }
+
+        static int penambahan(int a, int b)
+        {
+            return a + b;
+        }
+            
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
+
+        //TODO: tambahkan method untuk Perkalian dan Pembagian
+    }
+}
